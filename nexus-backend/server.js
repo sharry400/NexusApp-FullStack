ba7f8f00f3579a513b0d582b32dc9055c8d9e190
sharry400 <xi.am1pe28@gmail.com>
2026-06-23 17:33:31 +0500

    Deployment of Project

--- nexus-backend/server.js
+++ nexus-backend/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 });
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
